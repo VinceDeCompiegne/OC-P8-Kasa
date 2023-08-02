a06e6062e8d86928552e9b84bb7bee310de6413d
VinceDeCompiegne <vincent.deramaux@laposte.net>
2023-08-02 14:50:45 +0200

    JOUR 3 : animation sur le collapse

--- src/components/Collapse/index.jsx
+++ src/components/Collapse/index.jsx
@@ -3,26 +3,26 @@ import { useState  } from 'react'
 import '../../style/style.css'
 
 import FlecheH from "../../assets/images/FlecheH.svg"
-import FlecheB from "../../assets/images/FlecheB.svg"
 
 function Collapse({title,description}) {
 
-    const[Visible,setVisible] = useState (false)
-    const[Fleche,setFleche] = useState (FlecheB,[])
+    const[Visible,setVisible] = useState ("none",[])
+    const[Fleche,setFleche] = useState ("collapse-animation-fleche-h",[])
 
     const handleToggle = () => {
         setVisible((current) => !current);
-        if(Fleche===FlecheH){setFleche(FlecheB)}else{setFleche(FlecheH)}
+        if(Visible==="block"){setVisible("none")}else{setVisible("block")}
+        if(Fleche==="collapse-animation-fleche-h"){setFleche("collapse-animation-fleche-b")}else{setFleche("collapse-animation-fleche-h")}
     }
 
     return (
         <section className="collapse"> 
             <div onClick={handleToggle} className="collapse-bouton">
                 <p className="collapsible">{title}</p>
-                <img className="collapse-fleche" src={Fleche} alt="Indicateur d'ouverture du Collapse" />
+                <img className={`collapse-fleche ${Fleche}`} src={FlecheH} alt="Indicateur d'ouverture du Collapse" />
             </div>
 
-            <div className={`content ${Visible===false?"none":"block"}`}>
+            <div className={`content ${Visible}`}>
                 <p>{description}</p>
             </div>
 
