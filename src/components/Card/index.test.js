676f625e9f86c99b0368a27ea9654b8310c7f6e4
VinceDeCompiegne <vincent.deramaux@laposte.net>
2023-08-08 16:18:49 +0200

    KASA18 - Correction Jest test Card

--- src/components/Card/index.test.js
+++ src/components/Card/index.test.js
@@ -21,7 +21,7 @@ describe('Card', () => {
             </Fragment>
         )
         const jobArticle = screen.getByTestId(`job-link-card`)
-        expect(jobArticle.href).toBe("http://localhost/fiche/c67ab8a7")
+        expect(jobArticle.href).toMatch(new RegExp('fiche/c67ab8a7'))
         const jobImg = screen.getByTestId(`job-img-card`)
         expect(jobImg.src).toBe("https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-1.jpg")
         const jobH3 = screen.getByTestId(`job-h3-card`)
