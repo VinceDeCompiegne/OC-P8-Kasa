252c1d63cfccb6616dde84e1dd0fe980ab5b5716
VincentDeCompiegne <vincent.deramaux@laposte.net>
2023-09-21 14:24:32 +0200

    home route

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ root.render(
           <Route path="" element={<Home />} />
           <Route path="/fiche/:appart" element={<Fiche />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/OC-P8-Kasa" element={<Home />} />
           <Route path="/home/fiche/:appart" element={<Fiche />} />
           <Route path="/apropos" element={<Apropos />} />
           <Route path="/erreur" element={<Erreur404 />} />
